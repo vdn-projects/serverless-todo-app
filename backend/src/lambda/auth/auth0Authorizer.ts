6ac8a8a189f131bf64c6581ff4541f057a65e51f
Nguyen Van Duc <vanduc.dn@gmail.com>
2020-04-18 13:28:23 +0700

    Revise readme

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -9,10 +9,11 @@ import { JwksClient } from '../../auth/JwksClient';
 
 const logger = createLogger('auth');
 
+// 
 // TODO: Provide a URL that can be used to download a certificate that can be used
 // to verify JWT token signature.
 // To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = 'https://dev-hqrad4fi.eu.auth0.com/.well-known/jwks.json';
+const jwksUrl = 'https://dev-505-gusl.auth0.com/.well-known/jwks.json';
 const jwksClient = new JwksClient(jwksUrl);
 
 function getToken(authHeader: string): string {
